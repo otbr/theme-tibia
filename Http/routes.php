90dadb501912d01616b488d73ea8d998083fb60a
Kristoffer Eklund <kristoffereklund1@gmail.com>
2015-06-13 20:01:38 +0200

    News logic

--- Http/routes.php
+++ Http/routes.php
@@ -3,10 +3,7 @@
 require_once apolune_path('Account/Http/routes.php');
 require_once apolune_path('Support/Http/routes.php');
 require_once apolune_path('Library/Http/routes.php');
-
-$router->get('/', function () {
-    return redirect('/account');
-});
+require_once apolune_path('News/Http/routes.php');
 
 $router->group(['namespace' => 'pandaac\ThemeTibia\Http\Controllers'], function ($router) {
     $router->get('/api/name', 'ApiController@suggestName');
--- resources/views/news/latest.blade.php
+++ resources/views/news/latest.blade.php
@@ -1,5 +1,8 @@
 @extends('theme::app')
 
+@section('title', 'News')
+@section('body', 'news-latest')
+
 @section('content')
 <div class="box">
     <span class="corners top"></span>
@@ -9,7 +12,7 @@
     </header>
 
     <div class="inner-box-border">
-        <div class="inner-box">
+        <div class="inner-box fluid">
             Lorem ipsum.
         </div>
     </div>
@@ -25,7 +28,7 @@
     </header>
 
     <div class="inner-box-border">
-        <div class="inner-box">
+        <div class="inner-box fluid">
             Lorem ipsum.
         </div>
     </div>
@@ -42,7 +45,23 @@
 
     <div class="inner-box-border">
         <div class="inner-box">
-            Lorem ipsum.
+            
+            <div class="news-block">
+                <div class="headline">
+                    <div class="icon"></div>
+                    <div class="date">May 24 2015 -</div>
+                    <div class="title">At the Bottom of the Sea</div>
+                </div>
+                <div class="body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+
         </div>
     </div>
 
