678c1b119242d6edf58013d21c37cdc426cf494f
Christopher Eklund <eklundchristopher@gmail.com>
2015-07-27 01:04:32 +0200

    Updated gulp & dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,25 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
 
-elixir.config.assetsDir = 'resources/';
-elixir.config.publicDir = 'public/';
-elixir.config.cssOutput = elixir.config.publicDir + 'css/';
-elixir.config.jsOutput = elixir.config.publicDir + 'js/';
-
+elixir.config.assetsPath = 'resources/';
+elixir.config.publicPath = 'public/';
 elixir.config.production = true;
 elixir.config.sourcemaps = false;
+
+gulp.task('publishAssets', shell.task([
+    "php ../../../artisan vendor:publish --tag=public --force"
+]));
 
 elixir(function(mix) {
     mix
         .less('app.less')
-        .browserify('app.js', elixir.config.jsOutput + 'app.min.js')
-        .copy(elixir.config.assetsDir + 'img', elixir.config.publicDir + 'img')
-        .task('publish_assets')
+        .browserify('app.js', 'app.min.js')
+        .copy('resources/img', 'public/img')
         .version([
-            'css/app.css',
-            'js/app.min.js'
+            'public/css/app.css',
+            'public/js/app.min.js'
         ])
-        
-        .registerWatcher('copy', elixir.config.assetsDir + '/img/*')
-        .registerWatcher('publish_assets', elixir.config.publicDir + '/**/*');
-});
 
-gulp.task('publish_assets', shell.task([
-    "php ../../../artisan vendor:publish --tag=public --force"
-]));
+        .task('publishAssets', 'public/**/*')
+    ;
+});
