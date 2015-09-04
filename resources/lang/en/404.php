dfe0dbea94ae15b6dabf470fc8350d6a4e726781
Christopher Eklund <eklundchristopher@gmail.com>
2015-09-04 12:35:53 +0200

    404 page

---
+++ resources/lang/en/404.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'title'     => 'Page not found',
+    'heading'   => 'Oh no...',
+    'content'   => 'It appears you\'ve stumbled upon a page that no longer exists. If you believe 
+                    this is in error, please contact the site administrators and let them know.',
+    'back'      => 'Back',
+
+];
---
+++ resources/views/404.blade.php
@@ -0,0 +1,52 @@
+@extends('theme::app')
+
+@section('title', trans('theme::404.title'))
+@section('body', 'page-not-found 404')
+
+@section('content')
+
+    <div class="box">
+        <span class="corners top"></span>
+
+        <header class="header">
+            <div class="headline">{!! trans('theme::404.title') !!}</div>
+        </header>
+
+        <div class="inner-box-border">
+            <div class="inner-box">
+
+                <div class="table">
+                    <header class="header">
+                        <div class="borders">
+                            <span class="edges top"></span>
+                            {!! trans('theme::404.heading') !!}
+                            <span class="edges bottom"></span>
+                        </div>
+                    </header>
+
+                    <div class="content dark">
+                        <p>
+                            {!! trans('theme::404.content') !!}
+                        </p>
+                    </div>
+                </div>
+
+                <br>
+
+                <table class="full-width">
+                    <tr>
+                        <td class="text-center">
+                            <a href="{{ url('/') }}" class="blue-button">
+                                <span>{!! trans('theme::404.back') !!}</span>
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+
+        <span class="corners bottom"></span>
+    </div>
+    
+@stop
